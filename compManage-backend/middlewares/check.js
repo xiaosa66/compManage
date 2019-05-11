fa78ec2134602e6a054bccec4659f15e39c0f3f2
潇洒 <liuxiaosa@higohappy.com>
2019-05-11 21:23:52 +0800

    MOD :增加验证中间件

--- compManage-backend/middlewares/check.js
+++ compManage-backend/middlewares/check.js
@@ -1,31 +1,10 @@
-module.exports = {
-    // 已经登录了
-    checkNotLogin: (ctx) => {
-        if (ctx.session && ctx.session.user) {
-            // ctx.redirect('/posts');
-            console.log('already logged session=:', ctx.session.user);
-            return false;
-        }
-        return true;
-    },
-
-    // 已经登录了
-    whetherLogin: (ctx, next) => {
+async function check(ctx, next) {
         if (ctx.session && ctx.session.user) {
             console.log('登陆了:', ctx.session);
-            next();
+            await next();
         } else {
             console.log('未登录,跳转到登录');
-            ctx.redirect('/signin');
+            ctx.redirect('http://localhost:8002/#/');
         }
-    },
-
-    //没有登录
-    checkLogin: (ctx) => {
-        if (!ctx.session || !ctx.session.user) {
-            ctx.redirect('/signin');
-            return false;
-        }
-        return true;
-    }
 }
+module.exports = check;
