035dd9cee49f19e9cf32a7cf655afff1ad5ce352
潇洒 <liuxiaosa@higohappy.com>
2019-05-11 21:06:50 +0800

    MOD :想去掉浏览器缓存 没去掉......

--- compManage-frontend/config/fetch.js
+++ compManage-frontend/config/fetch.js
@@ -1,6 +1,6 @@
 const baseUrl = 'localhost:3000';
 
-export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
+export default async (url = '', data = {}, type = 'GET', method = 'fetch') => {
     type = type.toUpperCase();
     url = baseUrl + url;
 
@@ -9,11 +9,12 @@ export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
         Object.keys(data).forEach(key => {
             dataStr += key + '=' + data[key] + '&';
         })
-
         if (dataStr !== '') {
             dataStr = dataStr.substr(0, dataStr.lastIndexOf('&'));
             url = url + '?' + dataStr;
         }
+        url += "?timestamp=" + Math.random().toString(36).substr(2);
+        console.log('url',url);
     }
 
     if (window.fetch && method == 'fetch') {
@@ -35,6 +36,7 @@ export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
         }
 
         try {
+            url += "?timestamp=" + Math.random().toString(36).substr(2);
             const response = await fetch(url, requestConfig);
             const responseJson = await response.json();
             return responseJson
@@ -54,9 +56,12 @@ export default async(url = '', data = {}, type = 'GET', method = 'fetch') => {
             if (type == 'POST') {
                 sendData = JSON.stringify(data);
             }
-
+            url += "?timestamp=" + Math.random().toString(36).substr(2);
             requestObj.open(type, url, true);
             requestObj.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            // 禁用浏览器缓存
+            requestObj.setRequestHeader("Cache-Control", "no-cache");
+            requestObj.setRequestHeader("If-Modified-Since", "0");
             requestObj.send(sendData);
 
             requestObj.onreadystatechange = () => {
